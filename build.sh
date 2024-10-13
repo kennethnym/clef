29de0aa47df130bd136afae3aaf79f16b73d40e4
Kenneth <kennethnym@outlook.com>
2024-10-13 18:29:42 +0100

    chore: tidy up build.sh

--- build.sh
+++ build.sh
@@ -20,32 +20,15 @@ rapidxml_version="1.13"
 rapidxml_flags="-I$root/vendor/rapidxml-$rapidxml_version"
 
 skia_include="$root/vendor/skia"
-skia_link="-L$root/vendor/skia/out/Static/libaudioplayer.a\
-	-L$root/vendor/skia/out/Static/libbentleyottmann.a\
-	-L$root/vendor/skia/out/Static/libcompression_utils_portable.a\
-	-L$root/vendor/skia/out/Static/libperfetto.a\
-	-L$root/vendor/skia/out/Static/libdng_sdk.a\
-	-L$root/vendor/skia/out/Static/libpathkit.a\
-	-L$root/vendor/skia/out/Static/libpiex.a\
-	-L$root/vendor/skia/out/Static/libskcms.a\
-	-L$root/vendor/skia/out/Static/libskottie.a\
-	-L$root/vendor/skia/out/Static/libskparagraph.a\
-	-L$root/vendor/skia/out/Static/libskresources.a\
-	-L$root/vendor/skia/out/Static/libsksg.a\
-	-L$root/vendor/skia/out/Static/libskshaper.a\
-	-L$root/vendor/skia/out/Static/libskunicode_core.a\
-	-L$root/vendor/skia/out/Static/libskunicode_icu.a\
-	-L$root/vendor/skia/out/Static/libsvg.a\
-	-L$root/vendor/skia/out/Static/libwuffs.a\
-	-L$root/vendor/skia/out/Static/libskia.a"
-skia_flags="-I$skia_include -DSK_GL -DSK_GANESH"
+skia_link="-L$root/vendor/skia/out/Static -lskia"
+skia_flags="$skia_link -I$skia_include -DSK_GL -DSK_GANESH"
 
 glfw_flags=$(pkg-config --cflags --libs glfw3)
 gl_flags=$(pkg-config --cflags --libs gl)
 
 misc_lib_flags=$(pkg-config --cflags --libs libwebp libwebpmux libwebpdemux libpng libjpeg fontconfig)
 
-common_opts="$rapidxml_flags $skia_flags $glfw_flags $gl_flags -I$root/src -Wall --std=c++20 -L$root/vendor/skia/out/Static -lskia -lz $misc_lib_flags"
+common_opts="$rapidxml_flags $skia_flags $glfw_flags $gl_flags -I$root/src -Wall --std=c++20 -lz $misc_lib_flags"
 debug_opts="--debug -g --optimize -DDEBUG $common_opts"
 
 mkdir -p build
